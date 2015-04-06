984ecd5fdbfe7d6c1c5c2b417dcbd2da4ff12f2e
Liran Tal <liran.tal@gmail.com>
2015-04-06 08:43:18 +0300

    updating smtp server settings and extending events for extracting mail envelopes as they are coming in from users

--- server/config/simplesmtp.js
+++ server/config/simplesmtp.js
@@ -9,9 +9,31 @@ var config = require('./environment');
 
 // Setup SMTP server
 var simplesmtp = require('simplesmtp');
-var smtpServer = simplesmtp.createServer();
+var mailMessages = {};
 
-smtpServer.listen(config.smtp.port, function(err) {
+var smtpServer = simplesmtp.createServer({SMTPBanner: "Liran's NodeJS SMTP Watch Server", 
+	requireAuthentication: false, enableAuthentication: false, disableDNSValidation: true, validate: false},
+	function(req) {
+		req.accept();
+	}
+);
+
+smtpServer.listen(config.smtp.port, function(err){
 	if (err)
 		throw err;
+});
+
+smtpServer.on("data", function(envelope, chunk) {
+	var messageKey = envelope.date.getTime() + "-" + envelope.from;
+	var messageKeyEntry = mailMessages[messageKey];
+	messageKeyEntry.chunk = messageKeyEntry.chunk + chunk;
+	mailMessages[messageKey] = messageKeyEntry;
+
+
+});
+
+
+smtpServer.on("startData", function(envelope) {
+	var messageKey = envelope.date.getTime() + "-" + envelope.from;
+	mailMessages[messageKey] = { 'envelope':envelope, 'chunk': ''} ;
 });
